0bc74e0e39fbff9ca166f7c44741e98ee3aa3021
rutstyle <rutstyle@163.com>
2019-08-16 15:41:42 +0800

    add asyncComponent

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
-import IndexComponent from "./pages/home/index/index";
+import asyncComponent from "./components/asyncComponent";
+// import IndexComponent from "./pages/home/index/index";
+const IndexComponent = asyncComponent(() => import('./pages/home/index/index'));
 
 export default class RouterComponent extends Component {
     render() {
