e67a32f7b423f243c491235ae2bcc72d49eb5ce6
rutstyle <rutstyle@163.com>
2019-08-19 22:31:47 +0800

    获取Search查询参数

--- src/pages/home/goods/items.js
+++ src/pages/home/goods/items.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-export default class ItemsComponent extends React.Component{
-    render(){
-        return(
+export default class ItemsComponent extends React.Component {
+    componentWillReceiveProps(newProps) {
+        let cid = new URLSearchParams(newProps.location.search).get('cid');
+        console.log("cid", cid);
+    }
+
+    render() {
+        return (
             <div>商品详情页面</div>
         );
     }
