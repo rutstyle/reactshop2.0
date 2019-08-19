ad871b4e2f768e0284c3150efa30bacf54f822bc
rutstyle <rutstyle@163.com>
2019-08-19 23:55:10 +0800

    集成antd-mobile

--- src/pages/home/goods/items.js
+++ src/pages/home/goods/items.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Modal, Button, WhiteSpace, WingBlank, Toast } from 'antd-mobile';
 
 export default class ItemsComponent extends React.Component {
     state = {};
+
+    showAlert = () => {
+        const alert = Modal.alert;
+        const alertInstance = alert('', '确认要删除吗？', [
+            { text: '取消', onPress: () => console.log('cancel'), style: 'default' },
+            { text: '确认', onPress: () => console.log('ok') },
+        ]);
+        setTimeout(() => {
+            // 可以调用close方法以在外部close
+            console.log('auto close');
+            alertInstance.close();
+        }, 500000);
+    };
 
     componentWillReceiveProps(newProps) {
         let cid = new URLSearchParams(newProps.location.search).get('cid');
@@ -10,7 +24,10 @@ export default class ItemsComponent extends React.Component {
 
     render() {
         return (
-            <div>商品详情页面{this.state.cid}</div>
+            <div>
+                <div>商品详情页面{this.state.cid}</div>
+                <Button onClick={this.showAlert}>customized buttons</Button>
+            </div>
         );
     }
 }
