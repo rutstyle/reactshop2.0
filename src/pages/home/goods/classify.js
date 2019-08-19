70eea362eaff4cab218e94c9e903d5525b4ecd09
rutstyle <rutstyle@163.com>
2019-08-19 15:24:19 +0800

    添加iScroll库

--- src/pages/home/goods/classify.js
+++ src/pages/home/goods/classify.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+import IScroll from "../../../assets/js/libs//iscroll-lite.min";
 import config from "../../../assets/js/conf/config";
 import Css from "../../../assets/css/home/goods/classify.css";
 
 import ItemsComponent from "./items";
 
 export default class ClassifyComponent extends React.Component {
-    goBack(){
+    componentDidMount() {
+        this.resolveScroll();
+    }
+
+    goBack() {
         this.props.history.goBack();
+    }
+
+    resolveScroll() {
+        document.getElementById("scroll-classify").addEventListener("touchmove", (e) => {
+            e.preventDefault();
+        }, false);
+
+        new IScroll('#scroll-classify', {
+            scrollX: false,
+            scrollY: true,
+            preventDefault: false
+        });
     }
 
     render() {
@@ -18,36 +34,37 @@ export default class ClassifyComponent extends React.Component {
                     <span className={Css['icon-back']} onClick={this.goBack.bind(this)}></span>
                 </div>
                 <div className={Css['goods-main']}>
-                    <div className={Css['classify-wrap']}>
-                        <div className={Css['classify-item'] + " " + Css['active']}>潮流女装</div>
-
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
-                        <div className={Css['classify-item']}>潮流女装</div>
+                    <div id="scroll-classify" className={Css['classify-wrap']}>
+                        <div id="wrap">
+                            <div className={Css['classify-item'] + " " + Css['active']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                            <div className={Css['classify-item']}>潮流女装</div>
+                        </div>
                     </div>
                     <div className={Css['goods-conent']}>
                         <Switch>
